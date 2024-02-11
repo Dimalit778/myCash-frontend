2e95600068b8a0e2b7c1c5b025c7c091afd15c18
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-02-11 16:59:36 +0200

    a

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 import store from './store';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <HelmetProvider context={'MyCash'}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HelmetProvider>
 );

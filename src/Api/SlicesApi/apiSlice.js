ebad80bdd933f210245dbcd1325f4934c99362d9
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-02-11 18:39:50 +0200

    d

--- src/Api/SlicesApi/apiSlice.js
+++ src/Api/SlicesApi/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: '',
+  baseUrl: 'https://mycash-ra2a-yxco.onrender.com',
 });
 
 export const apiSlice = createApi({
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 import store from './store';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = createRoot(document.getElementById('root'));
 
